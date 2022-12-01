76a6b840b4fbe89d6a8df5cef2534a757ac97388
dpmanek <dpmanek@gmail.com>
2022-12-01 08:58:40 -0500

    adding redux

---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,47 @@
+import { v4 as uuid } from 'uuid';
+const initalState = [
+	{
+		id: uuid(),
+		name: 'Deep Manek',
+		email: 'dpmanek@gmail.com',
+		post: [],
+		followers: [],
+		following: [],
+		description: '',
+	},
+];
+
+let copyState = null;
+let index = undefined;
+
+const userReducer = (state = initalState, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		case 'CREATE_USER':
+			console.log('payload', payload);
+			return [
+				...state,
+				{
+					id: uuid(),
+					name: payload.name,
+					email: payload.email,
+					post: [],
+					followers: [],
+					following: [],
+					description: '',
+				},
+			];
+
+		case 'DELETE_USER':
+			copyState = [...state];
+			index = copyState.findIndex((x) => x.id === payload.id);
+			copyState.splice(index, 1);
+			return [...copyState];
+
+		default:
+			return state;
+	}
+};
+
+export default userReducer;
