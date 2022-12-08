4478203bf00bcfaca072c00928c21fbd006217b7
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-12-07 21:56:41 -0500

    redirect if user is not logged in

---
+++ client/src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div class="text-center">
+  <div class="spinner-border" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+</div>
+  )
+}
---
+++ client/src/services/useAuthStatus.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    console.log(auth);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+  return { loggedIn, checkingStatus };
+}
+
+// this is used to check if the user is logged in and authenticated
