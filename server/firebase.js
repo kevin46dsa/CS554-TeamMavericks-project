041158e52fa2107a2227664d1389b00bba10f7d5
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-12-14 14:28:45 -0500

    created dotenv file for service account

--- server/firebase.js
+++ server/firebase.js
@@ -1,11 +1,13 @@
 var admin = require("firebase-admin");
 //const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 // Fetch the service account key JSON file contents
-var serviceAccount = require("./instabuzz-325f2-firebase-adminsdk-t7kea-2494f3ce27.json");
+require('dotenv').config()
 
+//var serviceAccount = require("./instabuzz-325f2-firebase-adminsdk-t7kea-2494f3ce27.json");
+var serviceAccount = process.env.SERVICE_APP
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(serviceAccount)),
   // The database URL depends on the location of the database
   databaseURL: "https://instabuzz-325f2-default-rtdb.firebaseio.com"
 });
