e116083667a726274d06969a1e88582e23bf43af
dpmanek <dpmanek@gmail.com>
2022-12-14 21:56:41 -0500

    adding upload image functionality - frontend to backend- multer update

--- client/src/Components/Uploadimage/UploadImage.jsx
+++ client/src/Components/Uploadimage/UploadImage.jsx
@@ -1,114 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import {
-    getStorage,
-    ref,
-    uploadBytesResumable,
-    getDownloadURL,
-  } from "firebase/storage";
-  import { getAuth } from "firebase/auth";
-  import { v4 as uuidv4 } from "uuid";
-  import { addDoc, collection, serverTimestamp } from "firebase/firestore";
- 
-  import { useNavigate } from "react-router-dom";
-  import { db } from "../../firebase"
-  
 
-  const UploadImage = () => {
-	const navigate = useNavigate();
-  const auth = getAuth();
-  const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({
-    images: {},
-  });
-  const {
-    images,
-  } = formData;
-  
-  function onChange(e) {
-    let boolean = null;
-    if (e.target.value === "true") {
-      boolean = true;
-    }
-    if (e.target.value === "false") {
-      boolean = false;
-    }
-    // Files
-    if (e.target.files) {
-      setFormData((prevState) => ({
-        ...prevState,
-        images: e.target.files,
-      }));
-    }
-    // Text/Boolean/Number
-    if (!e.target.files) {
-      setFormData((prevState) => ({
-        ...prevState,
-        [e.target.id]: boolean ?? e.target.value,
-      }));
-    }
-  }
-  async function onSubmit(e) {
-    e.preventDefault();
-    setLoading(true);
-    
-    if (images.length > 6) {
-      setLoading(false);
-      alert("maximum 6 images are allowed");
-      return;
-    }
+const App = () => {
+	const [image, uploadImage] = useState('');
 
-    const data = new FormData();
-    [...images].map((image) => {
-      console.log(image) 
-      data.append(`file`, image);
-      
-  })
+	const setImage = (event) => {
+		const data = new FormData();
+		data.append('file', event.target.files[0]);
+		uploadImage(data);
+	};
 
-    axios.post('/upload', data).then(()=>{
-      setLoading(false);
-      alert("Listing created");
-    })
-      
-
-   
-   
-  }
+	const uploadHandle = (event) => {
+		axios.post(' http://localhost:8000/data/upload', image).then((res) => {
+			console.log('done');
+		});
+	};
 
 	return (
-        <div>
-			<h1>uploadImage</h1>
-        
-         
-         <form onSubmit={onSubmit}>
-           
-           <div >
-             <p >
-               The first image will be the cover (max 6)
-             </p>
-             <label for="formFileMultiple" class="form-label">Upload Images</label>
-             <input
-               type="file"
-               id="images"
-               onChange={onChange}
-               accept=".jpg,.png,.jpeg,.webp"
-               multiple
-               required
-               className="form-control"
-             />
-           </div>
-           <br/>
-           <button
-             type="submit"
-             className="btn btn-success"
-           >
-             Create Listing
-           </button>
-         </form>
-         
-       
-       </div>
-    )
+		<>
+			<h1>Upload file</h1>
+			<form onSubmit={uploadHandle}>
+				<input type="file" name="file" onChange={setImage} />
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	);
 };
 
-export default UploadImage;
+export default App;
