68c003b1b9f1a441dff0185dfe659c35960023a4
dpmanek <dpmanek@gmail.com>
2022-12-14 22:29:00 -0500

    profile and search here

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './Header.css';
-import {
-	NavLink,
-	useNavigate,
-} from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import useUser from '../../hooks/useUser';
 import { getAuth } from 'firebase/auth';
-
 
 const Login = () => {
 	return (
@@ -21,27 +17,30 @@ const Login = () => {
 	);
 };
 const Logout = (props) => {
-	let auth = props.auth
-	let navigate = props.navigate
+	let auth = props.auth;
+	let navigate = props.navigate;
 	function onLogout() {
-	
-		auth.signOut().then(()=>{
-		alert("User Signed out")
-		navigate("/login");
-		}).catch(()=>{
-		alert("Error with signning out")
-		})
-		
-	  }
-	
+		auth
+			.signOut()
+			.then(() => {
+				alert('User Signed out');
+				navigate('/login');
+			})
+			.catch(() => {
+				alert('Error with signning out');
+			});
+	}
+
 	return (
-		<button
-			className="text__button"
-			onClick={onLogout}
-			
-		>
-			Logout
-		</button>
+		<>
+			<h1>Search here</h1>
+			<button className="text__button" onClick={onLogout}>
+				Logout
+			</button>
+			<NavLink className="navlink" to="/profile">
+				<button className="text__button">Profile</button>
+			</NavLink>
+		</>
 	);
 };
 
@@ -49,8 +48,8 @@ const Header = () => {
 	const { user, isLoading } = useUser();
 	console.log(user);
 	const auth = getAuth();
-    const navigate = useNavigate();
-	
+	const navigate = useNavigate();
+
 	return (
 		<div className="app">
 			<div className="app__header">
@@ -63,12 +62,8 @@ const Header = () => {
 					</NavLink>
 
 					<div className="app__headerButtons">
-						{!user ? <Login /> : <Logout auth={auth} navigate={navigate}/>}
-						<NavLink className="navlink" to="/profile">
-							<button className="text__button">Profile</button>
-						</NavLink>
+						{!user ? <Login /> : <Logout auth={auth} navigate={navigate} />}
 					</div>
-					
 				</div>
 			</div>
 		</div>
