72a0550d8c2fe05c0c6347abcf321a817a285b51
dpmanek <dpmanek@gmail.com>
2022-12-16 16:24:22 -0500

    sending data for Upload post in imageUpload

--- client/src/Components/Uploadimage/UploadImage.jsx
+++ client/src/Components/Uploadimage/UploadImage.jsx
@@ -1,33 +1,46 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-
 const App = () => {
 	const [image, uploadImage] = useState('');
 
 	const setImage = (event) => {
 		const data = new FormData();
+
 		data.append('file', event.target.files[0]);
 		uploadImage(data);
 	};
 
-	const uploadHandle = (event) => {
-		axios.post(' http://localhost:8000/data/upload', image).then((res) => {
-			console.log('done');
-		});
+	const uploadHandle = async () => {
+		let data = {
+			uuid: '123234',
+			name: 'Deep',
+		};
+		axios
+			.post(' http://localhost:8000/data/upload2', data)
+			.then((res) => {
+				console.log('Executed 1st block');
+				axios.post(' http://localhost:8000/data/upload', image).then((res) => {
+					console.log('done');
+				});
+			})
+			.catch((e) => {
+				console.log(e);
+			});
 	};
 
 	return (
 		<>
 			<h1>Upload file</h1>
-			
+
 			<form className="post-form" onSubmit={uploadHandle}>
-			<div className="create-post">
-				<input  type="file" name="file" onChange={setImage}/>
-				<button className="post-button" type="submit">Post</button>
-			</div>
+				<div className="create-post">
+					<input type="file" name="file" onChange={setImage} />
+					<button className="post-button" type="submit">
+						Post
+					</button>
+				</div>
 			</form>
-			
 		</>
 	);
 };
